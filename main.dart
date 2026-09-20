a4d9855d97548ebfa81b2fba3f57db6e46ac00dc
ren896 <renaldiktp123@gmail.com>
2026-09-20 14:05:54 +0700

    Initial project

---
+++ main.dart
@@ -0,0 +1,12 @@
+void main() {
+
+  int umur=20;
+  String nama="renal";
+  double tinggi=163.5;
+  bool aktif=true;
+
+  print("Nama: $nama");
+  print("Umur: $umur");
+  print("Tinggi: $tinggi");
+  print("Aktif: $aktif");
+}
